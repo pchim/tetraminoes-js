73c13187d1f59f97f49a5c3796b75e4cb39b6c77
Peter Chim <pchim24@gmail.com>
2016-07-26 01:52:05 -0700

    Allow movement to the right

--- tetris.js
+++ tetris.js
@@ -29,7 +29,7 @@ class GridBox {
   toggle(state) {
     if (state) {
       this.on = true;
-      this.display = 'x';
+      this.display = '-';
     } else {
       this.on = false;
       this.display = '0';
@@ -48,7 +48,7 @@ class GridBox {
 
 // inintialize grid dimensions
 const NUM_COLS = 5;
-const NUM_ROWS = 5;
+const NUM_ROWS = 10;
 // grid and objects creator
 const createGrid = (numRows, numCols) => {
   let grid = [];
@@ -68,12 +68,39 @@ const createGrid = (numRows, numCols) => {
 
 const [grid, gridBoxes] = createGrid(NUM_COLS, NUM_ROWS);
 // we have separated out the shape moving logic from the grid
+// can refactor the shape to be a class
 let shape = [-NUM_COLS, -NUM_COLS + 1, -2 * NUM_COLS + 1, -2 * NUM_COLS];
+
+const checkKeyPress = (shape, key) => {
+  let wall = false;
+  let moveSpace = 1;
+  // for RIGHT key presses
+  const checkRight = (shapeNum) => (shapeNum % NUM_COLS + 1) < NUM_COLS;
+  
+  // check bounds
+  for (let i = 0; i < shape.length; i++) {
+    if (!checkRight(shape[i])) {
+      wall = true;
+    }
+  }
+  if (!wall) {
+    for (let i = 0; i < shape.length; i++) {
+      // can refactor shape to be  class and take care of itself
+      if (gridBoxes[shape[i]])
+        gridBoxes[shape[i]].step(grid);
+      shape[i] += moveSpace;
+      if (gridBoxes[shape[i]])
+        gridBoxes[shape[i]].activate(grid);
+      // can refactor to have collections
+    }    
+  }
+}
 
 // tradeoffs: have to recalculate gridboxnum every time
 // refactor: have shape be grid box number, subtract by num columns every time
 const step = () => {
   let touchFloor = false;
+  // can refactor each bound check to find the first invalid block
   for (let i = 0; i < shape.length; i++) {
     // check if the next location will be a floor
     if ( (shape[i] + NUM_COLS) > (NUM_ROWS*NUM_COLS - 1) ) {
@@ -87,7 +114,7 @@ const step = () => {
       shape[i] += NUM_COLS;
       // set the grid box to true and active
       if (gridBoxes[shape[i]]) {
-        gridBoxes[shape[i]].activate(grid);
+        gridBoxes[shape[i]].activate(grid, shape[i]);
       }
     } 
   } else {
@@ -97,7 +124,9 @@ const step = () => {
       }
     }    
   }
-
+  for (let i = 0; i < 10; i++) {
+    console.log('\n');
+  }
   console.log(grid);
   // update board 
   for (let i = 0; i < shape.length; i++) {
@@ -106,11 +135,11 @@ const step = () => {
     }
   }
   if (!touchFloor)
-    setTimeout(step, 800); 
+    setTimeout(step, 1000); 
 }
 
 step();
-
+checkKeyPress(shape, 'right');
 // TODO: Control grid front end using logic from gridbox objects
 // e.g. when to turn on grid
 // start off w/ brute force before optimizing
